f3497c65493c0431b70cc2641a896fc2a2d6035c
arturbasinski <artur.basinski@gmail.com>
2024-02-14 01:40:00 +0100

    install v1.2

--- install.ps1
+++ install.ps1
@@ -1,20 +1,25 @@
-# Check if $PROFILE exists
+# Check if the $PROFILE variable exists
+Write-Host "Checking if the \$PROFILE variable exists..."
 if (!$PROFILE) {
     $PROFILE = "$HOME\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
 }
 
-# Check if file $PROFILE exists
+# Check if the profile file exists
+Write-Host "Checking if the profile file exists..."
 if (!(Test-Path -Path $PROFILE)) {
+    Write-Host "Creating the profile file..."
     New-Item -ItemType File -Path $PROFILE -Force
 }
 
-# Check if folder Scripts exists
+# Check if the Scripts folder exists
+Write-Host "Checking if the Scripts folder exists..."
 $scriptsPath = Join-Path -Path (Split-Path -Path $PROFILE) -ChildPath 'Scripts'
 if (!(Test-Path -Path $scriptsPath)) {
+    Write-Host "Creating the Scripts folder..."
     New-Item -ItemType Directory -Path $scriptsPath
 }
 
-# URL list of scripts to download
+# List of URLs to the PowerShell scripts
 $scriptUrls = @(
     'https://raw.githubusercontent.com/arturbasinki/PScripts1/main/scripts/delete-type.ps1',
     'https://raw.githubusercontent.com/arturbasinki/PScripts1/main/scripts/view-type.ps1'
@@ -22,23 +27,28 @@ $scriptUrls = @(
 )
 
 foreach ($url in $scriptUrls) {
-    # Download script
+    Write-Host "Downloading the script from $url..."
+    # Download the script
     $scriptContent = (New-Object System.Net.WebClient).DownloadString($url)
 
-    # Write script as .ps1 in Scripts directory
+    # Save the script as a .ps1 file in the Scripts folder
+    Write-Host "Saving the script as a .ps1 file in the Scripts folder..."
     $scriptName = [System.IO.Path]::GetFileName($url)
     $destination = Join-Path -Path $scriptsPath -ChildPath $scriptName
     $scriptContent | Out-File -FilePath $destination
 
-    # Create alias
+    # Add an alias for the script to the profile file
+    Write-Host "Adding an alias for the script to the profile file..."
     $aliasName = [System.IO.Path]::GetFileNameWithoutExtension($url)
-    if (!(Get-Alias -Name $aliasName -ErrorAction SilentlyContinue)) {
-        Set-Alias -Name $aliasName -Value $destination
-    }
+    $aliasCommand = "Set-Alias -Name $aliasName -Value $destination"
+    Add-Content -Path $PROFILE -Value $aliasCommand
 }
 
-# Update variable $env:Path in $PROFILE file
+# Update the $env:Path variable in the profile file
+Write-Host "Updating the \$env:Path variable in the profile file..."
 $profileContent = Get-Content -Path $PROFILE
 if ($profileContent -notcontains "`$env:Path += `";$scriptsPath`"") {
     Add-Content -Path $PROFILE -Value "`$env:Path += `";$scriptsPath`""
 }
+
+Write-Host "The script installation process is complete."
